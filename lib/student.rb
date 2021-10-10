3437d4a8e3b05a7756f7710729d03ab6f9b7f703
Samantha Diaz <samidiaz3@gmail.com>
2021-10-10 18:44:57 -0400

    commit tree ca74ab84300c1ca67a79007a8a8d27552a7d1b69

--- lib/student.rb
+++ lib/student.rb
@@ -5,5 +5,89 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  def initialize (id = nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students;
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+
+  def save
+    
+    if self.id #is not nil/not empty...
+      self.update
+    else
+      
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?,?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  
+   end
+
+  def self.create (name, grade)
+    new_student = Student.new(name, grade)
+    new_student.save
+  end
+
+  def self.new_from_db(row)
+    #We assume this array contains elements in this order:  id, name, grade
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade) #self.new is the same as using Student.new
+    
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    #remember, .execute returns an array of arrays... need to map to get one
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+
+  def update
+    sql = <<-SQL
+      UPDATE students
+      SET name = ?, grade = ?
+      WHERE id = ?
+    SQL
+    #sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 
 end
